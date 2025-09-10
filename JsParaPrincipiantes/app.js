110e325c627b2c5489296fe9a55c8593da4db84a
RoblesMorenoMonicaMorela <124049812@upq.edu.mx>
2025-09-10 15:16:43 -0600

    hacemos uso de la estructura switch

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -1,11 +1,12 @@
-let score = 70;
+let typeCard = 'Credit Card';
 
-if(score > 30){
-    console.log('You need to practice more');
-}else if(score > 15){
-    console.log('Estas mejorando');
-}
-
-else{
-    console.log('You need to follow this tutorial');
+switch(typeCard){
+    case 'Debid Card':
+        console.log('This is a debid card');
+        break;
+    case 'Credit Card':
+        console.log('This is a credit card');
+        break;
+    default:
+        console.log('No Card');
 }
