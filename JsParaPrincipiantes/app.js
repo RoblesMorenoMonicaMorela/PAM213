384951159485291204a4c2913b8376726b350166
RoblesMorenoMonicaMorela <124049812@upq.edu.mx>
2025-09-10 14:40:42 -0600

    nos enseña a utilizar comentarios/etiquetas

--- JsParaPrincipiantes/app.js
+++ JsParaPrincipiantes/app.js
@@ -1,6 +1,18 @@
+//este código lo siguiente
 var nameuser = "john";
 let lastname = "carter";
 
+//reasignando el valor de la variable nameuser
+
 nameuser = 'pepe';
 
-console.log(nameuser);
+// cualquiercosas
+
+const PI = 3.1415;
+
+console.log(PI);
+
+// came1case
+
+let userName = 'gordon';
+
