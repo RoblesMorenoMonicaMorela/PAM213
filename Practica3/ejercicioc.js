b6e9bb2bebfe3312bfce2b12288eb3213ac9dda0
RoblesMorenoMonicaMorela <124049812@upq.edu.mx>
2025-09-25 15:12:33 -0600

    SE HACE USO DE TRY Y DE CATCH  PARA  COMPLETAR EL ASYNC PEDIDO POR EL PROFESOR(IMPRIMIMOS EL RESULTADO DE LA PROMESA)

---
+++ Practica3/ejercicioc.js
@@ -0,0 +1,17 @@
+function simularPeticionAPI (){
+    return new Promise(resolve=>{
+        setTimeout(()=>{
+            resolve("Datos recibidos correctamente");
+        },5000);
+    });
+}
+
+async function obtenerDatos() {
+    try {
+        const respuesta=await(simularPeticionAPI);
+        console.log("Resultado: ",respuesta);
+    } catch (error) {
+        console.error("Error al recibir los datos.");
+    }   
+}
+obtenerDatos();
