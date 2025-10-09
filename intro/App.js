88861827a570dfd8b7a687f2b1c3efa87b9cefe5
RoblesMorenoMonicaMorela <124049812@upq.edu.mx>
2025-10-08 19:35:55 -0600

    identificación de zonas y componentes

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,22 @@
+//1. imports: zona de importacones
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+//2. Main: Zona de componentes
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Hola Mundo React Native!!!</Text>
+
+      <Text>Hola Mundo React Native!!!</Text>{/*con shift+alt+a te dice cómo son los comentarios  */}
       <StatusBar style="auto" />
+
     </View>
   );
 }
 
+//3. Styles: Zona estética y posicionamiento
 const styles = StyleSheet.create({
   container: {
     flex: 1,
