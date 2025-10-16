c6c8c0ecbffd59a9c241f69040df78c3d60c8261
RoblesMorenoMonicaMorela <124049812@upq.edu.mx>
2025-10-15 19:40:44 -0600

    Ponemos contenido en BotonesScreen y modificamos App.js para que nos muestre MenuScreen en lugar de ContadorScreen

--- intro/App.js
+++ intro/App.js
@@ -1,13 +1,13 @@
 //1. imports: zona de importacones
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import ContadorScreen from './screens/ContadorScreen';
+import MenuScreen from './screens/MenuScreen';
 
 //2. Main: Zona de componentes
 export default function App() {
 
   return (
-  <ContadorScreen/>
+  <MenuScreen/>
 
   );
 }
---
+++ intro/screens/BotonesScreen.js
@@ -0,0 +1,11 @@
+import { Text, StyleSheet, View } from 'react-native'
+
+export default function BotonesScreen() {
+    return (
+      <View>
+        <Text>Próximamente por José María & equipo</Text>
+      </View>
+    )
+  }
+
+const styles = StyleSheet.create({})
